1fe706a18b0e2bdd22a928dd04cac74f47e6d236
JocY <109790602+JocY16@users.noreply.github.com>
2023-03-23 20:24:01 +0100

    Add files via upload

---
+++ ES-teleport/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'adamant' -- FX verzió (általában adamant, de van cerulean is, de az szokott buggolni)
+game 'gta5' -- GTA
+
+author 'JocY'
+version '1.0.0' -- Verziószám (teljesen mindegy, szöveget is írhatsz)
+description 'Szkript' -- Leírás
+
+client_scripts {
+    'client.lua'
+}
